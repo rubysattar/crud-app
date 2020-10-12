3fb48a4893d0c5e3e63c8663c0dfd6be639384ea
rubysattar <rubysattar@gmail.com>
2020-10-12 16:23:58 -0400

    testing out css file structure with thymeleaf

--- src/main/java/com/aquent/crudapp/Application.java
+++ src/main/java/com/aquent/crudapp/Application.java
@@ -3,7 +3,7 @@ package com.aquent.crudapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Inside this class, would a repository  for client and person
+// Inside this class, a repository for client and person may
 // need to be autowired?
 @SpringBootApplication
 public class Application {
